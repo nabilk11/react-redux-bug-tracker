fde56ad71b8d20e8f33a531a4e8b6efc72d26136
nabilk11 <khan.nabiln11@gmail.com>
2022-02-05 02:00:51 -0500

    configured Redux on th eindex.js file, redux now has state, and Provider has been added to the ReactDom render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import './index.css';
 import App from './App';
 
+// Reducers Import
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+// REDUX CONFIGURATION
+// combining reducers as there can only be one in the store - using combineReducers
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer,
+})
+// must configure the store for redux
+const store = configureStore({
+  reducer,
+});
+
 ReactDOM.render(
+  // must add Provider to the react app
+  <Provider store={store}/>,
     <App />,
   document.getElementById('root')
 );
