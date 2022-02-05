cf45639dd5520901d30874f28ef232cfc2d13ceb
nabilk11 <khan.nabiln11@gmail.com>
2022-02-05 03:53:06 -0500

    added change of state once logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Login from './Views/Login/login';
 import './App.css';
 
 function App() {
+// getting state inside useSelector, destructor 
+
+const {auth} = useSelector(state => state)
+
+
   return (
     <div className="App">
-      <Login />
+      
+      {!auth.LoggedIn ? <Login /> : //if the user is not loggedIn then render the Login page, then once they login render the next page
+      <h1>hello</h1> }
     </div>
   );
 }
