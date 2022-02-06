bbbb913da15ed0d7af931d2b038ae52100c65fe6
nabilk11 <khan.nabiln11@gmail.com>
2022-02-06 01:18:20 -0500

    added styling

--- src/Views/Components/Dashboard/Card.jsx
+++ src/Views/Components/Dashboard/Card.jsx
@@ -5,7 +5,7 @@ import PriorityController from '../../../Controllers/priorityController';
 export default function Card(props) {
     const {level, color} = PriorityController(props.priority);
   return (
-  <div className="dashboard-card" style={{color: color}}>
+  <div className="dashboard-card" onClick={props.clicked} style={{color: color}}>
       <h3>{level} Priority Bugs</h3>
       <p>{props.count}</p>
 
--- src/Views/Pages/Dashboard/Dashboard.jsx
+++ src/Views/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBugs } from '../../../Controllers/Redux/bugSlice';
+import { useHistory } from 'react-router-dom';
 import Card from '../../Components/Dashboard/Card';
+import './dashboard.css'
 
 export default function Dashboard() {
     const dispatch = useDispatch();
     const bugs = useSelector(state=>state.bugs);
+    const browserHistory = useHistory();
     let highCount = 0
     let midCount = 0
     let lowCount = 0
@@ -15,6 +18,10 @@ export default function Dashboard() {
         lowCount = filterBugs(3);
     }
 
+    function redirect() {
+        browserHistory.push('/viewbugs')
+    }
+
     function filterBugs(priority) {
         return bugs.filter((bug)=> {return bug.priority == priority})
     }
@@ -22,12 +29,12 @@ export default function Dashboard() {
         dispatch(getBugs())
     },[bugs == undefined])
   return (
-  <div className="bugs-container">
+  <div className="bugs-container" id="dashboard-container">
       <h1>unbugify.
         <small>dashboard<br />Bug Count</small></h1>
-<Card priority="1" count={highCount.length} />
-<Card priority="2" count={midCount.length}  />
-<Card priority="3" count={lowCount.length}  />
+<Card priority="1" count={highCount.length} clicked={redirect}/>
+<Card priority="2" count={midCount.length}  clicked={redirect}/>
+<Card priority="3" count={lowCount.length}  clicked={redirect}/>
   </div>
   )
 }
